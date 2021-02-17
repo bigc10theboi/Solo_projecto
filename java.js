d614ff64c5b9ddf95c8b7c30a8c1067acc7431fc
Carter Venne <you@example.com>
2021-02-17 15:58:37 -0600

    moment date functionality

--- java.js
+++ java.js
@@ -18,3 +18,16 @@ document.getElementById("pageTitle").addEventListener("click", function(event){
 // $.ajax(settings).done(function (response) {
 // 	console.log(response);
 // });
+
+
+//code for moment date
+var now = moment();
+var dateFormat = "MM/DDDD/YYYY";
+var convertedDate = moment(now, dateFormat);
+var newDate = moment();
+//function to display date//
+function displayCurrentDate() {
+  var currentDate = moment().format("MMMM, DD, YYYY");
+  $("#currentDay").text(currentDate);
+}
+displayCurrentDate();
